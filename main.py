03e61dec3bfdb0f4c2968c758edd71d1af50efc2
Chen <sheiner.chen@gmail.com>
2022-08-16 12:15:40 +0300

    add first code for dataset processing

--- main.py
+++ main.py
@@ -8,18 +8,11 @@ from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk.corpus import stopwords
 
 
-def data_preprocessing(text_path):
-    # Reads ‘alice.txt’ file
-    with open(text_path, encoding="mbcs") as sample:
-        s = sample.read()
-
-    # Replaces escape character with space
-    f = s.replace("\n", " ")
-
+def data_preprocessing(raw_file_lines):
     data = []
     stop_words = set(stopwords.words('english'))
     # iterate through each sentence in the file
-    for i in sent_tokenize(f):
+    for i in sent_tokenize(raw_file_lines):
         temp = []
         # tokenize the sentence into words and avoid stop words
         for j in word_tokenize(i):
@@ -33,21 +26,37 @@ def data_preprocessing(text_path):
     return data, flat_list
 
 
-db, words = data_preprocessing(r"C:\Users\Chen\Documents\Masters_degree\word2vec_vs_glove\alice.txt")
+# text_path = r"C:\Users\Chen\Documents\Masters_degree\word2vec_vs_glove\us_patent_data\train.csv"
 
-model_cbow, model_sg = word2vec_model.train_word2vec(db, 1, 365, 5)
-a = word2vec_model.check_one_word(model_cbow, "alice", words)
-b = word2vec_model.check_one_word(model_sg, "alice", words)
 
-z = pd.DataFrame({"word": words, "cbow": a, "skip_gram": b})
-print(z.head(5))
-z.sort_values("cbow", inplace=True)
-print(z.head(5))
+# db, words = data_preprocessing(f)
 
-print(model_cbow.wv.most_similar("accounting"))
-mean = z["cbow"].mean()
-below = z[z["cbow"] < mean]
-above = z[z["cbow"] > mean]
+# raw_data = pd.read_csv("Salary_Data.csv")
+
+"""
+Example of read and use database with word2vec:
+"""
+# text_path = r"C:\Users\Chen\Documents\Masters_degree\word2vec_vs_glove\alice.txt"
+# with open(text_path) as sample:
+#     s = sample.read()
+#     # Replaces escape character with space
+#     f = s.replace("\n", " ")
+# db, words = data_preprocessing(f)
+#
+# model_cbow, model_sg = word2vec_model.train_word2vec(db, 1, 365, 5)
+# a = word2vec_model.check_one_word(model_cbow, "alice", words)
+# b = word2vec_model.check_one_word(model_sg, "alice", words)
+#
+# z = pd.DataFrame({"word": words, "cbow": a, "skip_gram": b})
+# print(z.head(5))
+# z.sort_values("cbow", inplace=True)
+# print(z.head(5))
+#
+# print(model_cbow.wv.most_similar("accounting"))
+
+# mean = z["cbow"].mean()
+# below = z[z["cbow"] < mean]
+# above = z[z["cbow"] > mean]
 # plt.figure(figsize=(20, 12))
 # plt.xticks(rotation=90)
 # sns.barplot(x="word", y="cbow", data=z,
---
+++ patent_dataset.py
@@ -0,0 +1,30 @@
+from datasets import load_dataset
+# Pretty print
+from pprint import pprint
+
+dataset_dict = load_dataset('HUPD/hupd',
+                            name='sample',
+                            data_files="https://huggingface.co/datasets/HUPD/hupd/blob/main/hupd_metadata_2022-02-22.feather",
+                            icpr_label=None,
+                            train_filing_start_date='2016-01-01',
+                            train_filing_end_date='2016-01-21',
+                            val_filing_start_date='2016-01-22',
+                            val_filing_end_date='2016-01-31',
+                            )
+
+print('Loading is done!')
+
+# Dataset info
+print(dataset_dict)
+
+# Print dataset dictionary contents and cache directory
+print('Dataset dictionary contents:')
+pprint(dataset_dict)
+print('Dataset dictionary cached to:')
+pprint(dataset_dict.cache_files)
+
+# Print info about the sizes of the train and validation sets
+print(f'Train dataset size: {dataset_dict["train"].shape}')
+print(f'Validation dataset size: {dataset_dict["validation"].shape}')
+
+dataset_dict["train"][4]
